c49a7098d9070a92a0142e60f46b104956a47591
Simon Tran <thanhnhantran1501@gmail.com>
2023-06-07 00:18:45 -0700

    solve length of last word

---
+++ src/lengthOfLastWord.tsx
@@ -0,0 +1,23 @@
+// Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+// A word is a maximal
+// substring
+//  consisting of non-space characters only.
+
+// Example 1:
+
+// Input: s = "Hello World"
+// Output: 5
+// Explanation: The last word is "World" with length 5.
+
+//MY SOLUTION
+function lengthOfLastWord(s: string): number {
+  const arr = s.split(" ").filter((e) => Boolean(e) === true);
+  return arr[arr.length - 1].split("").length;
+}
+
+//BEST SOLUTION
+function lengthOfLastWordLc(s: string): number {
+  const arr = s.trim().split(" ").reverse();
+  return arr[0].split("").length;
+}
