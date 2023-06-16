35b508e6987b45723d6ba5c84ba68c9064b1a0bb
Simon Tran <thanhnhantran1501@gmail.com>
2023-06-16 16:19:52 -0700

    solve the sqrtProblem

---
+++ src/sqrtProblem69.ts
@@ -0,0 +1,10 @@
+// Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+// Input: x = 4
+// Output: 2
+// Explanation: The square root of 4 is 2, so we return 2.
+
+//TO CALCULATE SQUARE ROOT, WE USE MATH.SQRT AND WE ROUND THE NUMBER DOWN TO THE NEAREST INTERGER WITH MATH.FLOOR
+function mySqrt(x: number): number {
+  return Math.floor(Math.sqrt(x));
+}
